98f83c72b1c47a1a35503422d8a93b46251e88b6
deepakactowiz <deepakk.actowiz@gmail.com>
2026-05-25 18:17:54 +0530

    second commit

--- app.py
+++ app.py
@@ -14,5 +14,38 @@ def create_user():
 def user_details():
     return jsonify(users)
 
+@app.route("/user/<int:id>",methods=["PUT"])
+def update_user(id):
+    data = request.json
+    for each_user in users:
+        if each_user["id"]==id:
+            each_user["name"] = data["name"]
+            each_user["city"] = data["city"]
+            return jsonify({
+                "message":"User updated successfully...",
+                "data":users
+            })
+    return jsonify(
+        {
+            "message":"user not found..."
+        }
+    )
+@app.route("/user/<int:id>",methods=["DELETE"])
+def delete_user(id):
+    for each_user in users:
+        if each_user["id"]==id:
+            users.remove(each_user)
+            return jsonify(
+                {
+                    "message":"Data user deleted successfully...",
+                    "data" : users
+                }
+            )
+    return jsonify(
+        {
+            "message": "User not found"
+        }
+    )
+
 if __name__ == "__main__" :
     app.run(debug=True)
